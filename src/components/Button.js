8a06278ddf4d0588b95fbed29aa157353808a460
mikailbadoula <michaeldevfay@gmail.com>
2021-06-23 11:34:04 +0100

    Add gif and add multiple button styles

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-expressions */
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
@@ -6,12 +7,17 @@ const BtnStyle = styled.div`
   margin-top: 2rem;
   .button {
     font-size: 2.2.rem;
-    background-color: var(--black);
+    background-color: ${(props) => {
+      // eslint-disable-next-line no-unused-expressions
+      props.border ? 'transparent' : 'var(--black)';
+    }};
     padding: 0.7em 2em;
     border-radius: 8px;
     display: inline-block;
     border: 2px solid var(--black);
-    color: var(--light);
+    color: ${(props) => {
+      props.border ? 'var(--light)' : 'var(--black)';
+    }};
   }
   @media only screen and (max-width: 768px) {
     .button {
@@ -20,9 +26,9 @@ const BtnStyle = styled.div`
   }
 `;
 
-export default function Button({ btnLink }) {
+export default function Button({ btnLink, border }) {
   return (
-    <BtnStyle>
+    <BtnStyle border={border}>
       <Link className="button" to={btnLink}>
         See my work
       </Link>
--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import Avatar from '../assets/images/hero.png';
 import Button from './Button';
+import Banner from '../assets/images/banner.gif';
 
 export default function HeroSection() {
   return (
     <div>
       <div className="container">
         <div className="hero__img">
-          <img src={Avatar} alt="" />
+          <img src={Banner} alt="" />
         </div>
         <div className="hero__info">
-          <Button btnLink="/projects" />
+          <Button btnLink="/projects" border={false} />
         </div>
       </div>
     </div>
