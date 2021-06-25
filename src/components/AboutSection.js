7b1202643097dab270ba83c87e166d641b537019
mikailbadoula <michaeldevfay@gmail.com>
2021-06-25 14:27:24 +0100

    Add images

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -2,25 +2,30 @@ import React from 'react';
 import SectionTitle from './SectionTitle';
 import PText from './PText';
 import Button from './Button';
+import AboutImg from '../assets/images/about-sec-img.jpeg';
 
 export default function AboutSection() {
   return (
     <div>
       <div className="container">
-        <div className="aboutSection__left" />
-        <SectionTitle
-          className="section-title"
-          subheading="Allow me to introduce myself..."
-          heading="ABOUT ME"
-        />
-        <PText>
-          I am a web designer and developer based in London. I create UX-centred
-          websites and web apps. In my spare time, I enjoy hiking, reading and
-          practicing yoga.
-        </PText>
-        <div className="aboutSection__buttons">
-          <Button btnLink="/projects" btnText="My Work" />
-          <Button btnLink="/about" btnText="See More" border />
+        <div className="aboutSection__left">
+          <SectionTitle
+            className="section-title"
+            subheading="Allow me to introduce myself..."
+            heading="ABOUT ME"
+          />
+          <PText>
+            I am a web designer and developer based in London. I create
+            UX-centred websites and web apps. In my spare time, I enjoy hiking,
+            reading and practicing yoga.
+          </PText>
+          <div className="aboutSection__buttons">
+            <Button btnLink="/projects" btnText="My Work" />
+            <Button btnLink="/about" btnText="See More" border />
+          </div>
+        </div>
+        <div className="aboutSection__right">
+          <img className="AboutImg" src={AboutImg} alt="" />
         </div>
       </div>
     </div>
