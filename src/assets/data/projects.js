fa2472ee4282680db75699ab7f1404e522c40452
mikailbadoula <michaeldevfay@gmail.com>
2021-06-27 14:22:08 +0100

    Import project images and data

--- src/assets/data/projects.js
+++ src/assets/data/projects.js
@@ -1,41 +1,44 @@
 import { v4 as uuidv4 } from 'uuid';
-import ProjectImg from '../images/projectImg.png';
-import UTrackerImg from '../images/utracker.jpg';
-import GreenCtgImg from '../images/greenctg.jpg';
-import CoinTrackerImg from '../images/cointracker.jpg';
-import CavinImg from '../images/cavinimg.jpg';
+import ChummyImg from '../images/chummy.png';
+import MindfulImg from '../images/mindful.png';
+import TwaddleImg from '../images/twaddle.png';
+import DonQuizoteImg from '../images/donquizote.png';
+import FacAppSite from '../images/quotegenerator.png';
 
 const projects = [
   {
-    id: '',
-    name: '',
-    desc: '',
+    id: uuidv4(),
+    name: 'Chummy',
+    desc:
+      'A full-stack app built as a team of four for our final project at FAC.',
 
-    img: '',
+    img: ChummyImg,
   },
   {
-    id: '',
-    name: '',
-    desc: '',
-    img: '',
+    id: uuidv4(),
+    name: 'The Mindful Pomodoro',
+    desc:
+      'A Pomodoro Timer I built as a solo project using Vanilla JS, HTML and CSS',
+    img: MindfulImg,
   },
   {
-    id: '',
-    name: '',
-    desc: '',
-    img: '',
+    id: uuidv4(),
+    name: 'Twaddle',
+    desc: 'A server-side based blog app built collaboratively during FAC',
+    img: TwaddleImg,
   },
   {
-    id: '',
-    name: '',
-    desc: '',
-    img: '',
+    id: uuidv4(),
+    name: 'Don Quizote',
+    desc: 'A client-side based blog app built as a pair project using React',
+    img: DonQuizoteImg,
   },
   {
-    id: '',
-    name: '',
-    desc: '',
-    img: '',
+    id: uuidv4(),
+    name: 'Quote Generator – FAC Application',
+    desc:
+      'My application site for FAC – featuring a quote generator built using JS, HTML & CSS',
+    img: FacAppSite,
   },
 ];
 
