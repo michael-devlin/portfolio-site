436cfb7947dfe634986b17dcd70c79be30c42d9e
mikailbadoula <michaeldevfay@gmail.com>
2021-07-02 15:51:52 +0100

    add styling and functionality for nav carousel buttons

--- src/components/ProjectSec.js
+++ src/components/ProjectSec.js
@@ -28,6 +28,27 @@ const ProjectSecStyles = styled.div`
   .swiper-container {
     padding-top: 8rem;
   }
+  .swiper-button-prev,
+  .swiper-button-next {
+    position: absolute;
+    height: 50px;
+    width: 50px;
+    background-color: var(--bg);
+    z-index: 10;
+    right: 60px;
+    left: auto;
+    top: 0;
+    transform: translateY(50%);
+    color: var(--black);
+    border-radius: 8px;
+  }
+  .swiper-button-next {
+    right: 0;
+  }
+  .swiper-button-prev::after,
+  .swiper-button-next::after {
+    font-size: 2rem;
+  }
 `;
 export default function ProjectSec() {
   return (
