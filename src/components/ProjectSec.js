f6f77df281828d95a4f790f949cbbb931a766ef8
mikailbadoula <michaeldevfay@gmail.com>
2021-07-02 14:30:04 +0100

    add nav carousel

--- src/components/ProjectSec.js
+++ src/components/ProjectSec.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import { Swiper } from 'swiper/react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { Navigation } from 'swiper';
 import styled from 'styled-components';
 import SectionTitle from './SectionTitle';
 import projects from '../assets/data/projects';
 import ProjectsItems from './ProjectsItems';
+import 'swiper/swiper-bundle.min.css';
+
+SwiperCore.use([Navigation]);
 
 const ProjectSecStyles = styled.div`
+    padding: 10rem 0;
     background-color: white;
   }
   p {
@@ -14,6 +19,14 @@ const ProjectSecStyles = styled.div`
   }
   h2 {
     color: var(--black);
+  }
+  .projects__allItems {
+    display: flex;
+    gap: 3rem;
+    margin-top: 5rem;
+  }
+  .swiper-container {
+    padding-top: 8rem;
   }
 `;
 export default function ProjectSec() {
@@ -26,10 +39,14 @@ export default function ProjectSec() {
           subheading="Examples of some of my work"
         />
         <div className="projects__allItems">
-          <Swiper>
+          <Swiper spaceBetween={30} slidesPerView={2} navigation>
             {projects.map((project, index) => {
               if (index >= 5) return;
-              return <ProjectsItems />;
+              return (
+                <SwiperSlide key="project.id">
+                  <ProjectsItems />
+                </SwiperSlide>
+              );
             })}
           </Swiper>
         </div>
