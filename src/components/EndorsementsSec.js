d35e02dd9b0864c6f66a3ea35653626cd5762996
mikailbadoula <michaeldevfay@gmail.com>
2021-07-05 15:12:05 +0100

    Add text to endorsements section

--- src/components/EndorsementsSec.js
+++ src/components/EndorsementsSec.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { SwitchTransition, CSSTransition } from 'react-transition-group';
 import SectionTitle from './SectionTitle';
+import PText from './PText';
 
 export default function EndorsementsSec() {
   return (
@@ -9,6 +11,19 @@ export default function EndorsementsSec() {
           heading="Endorsements"
           subheading="what my colleagues have to say about my work"
         />
+        <div className="endorsements__wrapper">
+          <SwitchTransition>
+            <CSSTransition>
+              <div className="endorsements__info">
+                <div className="endorsements__desc">
+                  <PText>Text goes here.</PText>
+                </div>
+                <h2 className="endorsement__name">Michael Devlin</h2>
+                <p className="endorsement__title">Software Developer</p>
+              </div>
+            </CSSTransition>
+          </SwitchTransition>
+        </div>
       </div>
     </div>
   );
