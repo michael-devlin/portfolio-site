7da3053defb912370bfb7c4ff011a883047a0fbd
mikailbadoula <michaeldevfay@gmail.com>
2021-07-17 14:09:29 +0100

    add contact button

--- src/components/ContactBanner.js
+++ src/components/ContactBanner.js
@@ -1,5 +1,17 @@
 import React from 'react';
+import Button from './Button';
+import PText from './PText';
 
 export default function ContactBanner() {
-  return <div>This is the contact banner.</div>;
+  return (
+    <div>
+      <div className="container">
+        <div className="contactBanner__wrapper">
+          <PText>Have a query?</PText>
+          <h3 className="contactBanner__heading">Reach Out</h3>
+          <Button btnText="Contact Me" btnLink="/contact" />
+        </div>
+      </div>
+    </div>
+  );
 }
