f7fa8df306cd7b54365a1aa9c382b7d72e59800b
mikailbadoula <michaeldevfay@gmail.com>
2021-07-17 14:44:20 +0100

    add styles to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,21 +1,34 @@
 import React from 'react';
+import styled from 'styled-components';
 import PText from './PText';
 import FooterColumn from './FooterColumn';
 
+const FooterStyles = styled.div`
+  padding-top: 10rem;
+  background-color: var(--black);
+`;
+
 export default function Footer() {
   return (
-    <div className="container">
-      <div className="footer__column">
-        <h1 className="footer__column__title">Michael Devlin</h1>
-        <PText>
-          A web developer and designer based in London with an eye for UX and
-          accessible design.
-        </PText>
+    <FooterStyles>
+      <div className="container">
+        <div className="footer__column">
+          <h1 className="footer__column__title">Michael Devlin</h1>
+          <PText>
+            A web developer and designer based in London with an eye for UX and
+            accessible design.
+          </PText>
+        </div>
+        <div className="footer__column2">
+          <FooterColumn />
+        </div>
+        <div className="footer__column3">
+          <FooterColumn />
+        </div>
+        <div className="footer__column4">
+          <FooterColumn />
+        </div>
       </div>
-      <div className="footer__column2" />
-      <FooterColumn />
-      <div className="footer__column3" />
-      <div className="footer__column4" />
-    </div>
+    </FooterStyles>
   );
 }
