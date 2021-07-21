5a624fdea834db44a8446e8dea9deb99ef3dae3d
mikailbadoula <michaeldevfay@gmail.com>
2021-07-21 12:26:07 +0100

    add footer column styles

--- src/components/FooterColumn.js
+++ src/components/FooterColumn.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+const ColumnStyle = styled.div`
+  .heading {
+    font-size: 2.4rem;
+    margin-bottom: 2rem;
+  }
+  li {
+    margin-bottom: 1rem;
+  }
+  a {
+    font-size: 1.8rem;
+  }
+`;
+
 export default function FooterColumn({
   heading = 'column heading',
   links = [
