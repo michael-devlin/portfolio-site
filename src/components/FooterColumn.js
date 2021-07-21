45368b87576d42612bba8147507dce91991b3c8b
mikailbadoula <michaeldevfay@gmail.com>
2021-07-21 12:09:32 +0100

    add column heading prop to component

--- src/components/FooterColumn.js
+++ src/components/FooterColumn.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
-export default function FooterColumn() {
-  return <div>This is the footer column.</div>;
+export default function FooterColumn({ heading = 'column heading' }) {
+  return (
+    <div>
+      <h1 className="heading">{heading}</h1>
+    </div>
+  );
 }
