523a3a737988e0fa9139dce82bf41a17054408aa
mikailbadoula <michaeldevfay@gmail.com>
2021-08-24 09:30:44 +0100

    add styles to projects list items on projects page

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -7,6 +7,31 @@ import ProjectInfo from '../assets/data/projects';
 
 const ProjectStyle = styled.div`
   padding: 10rem 0;
+  .projects__allItems {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+    gap: 5rem;
+    margin-top: 5rem;
+  }
+  .projects__searchBar {
+    position: relative;
+    width: 300px;
+    margin-top: 5rem;
+  }
+  .projects__searchBar input {
+    width: 100%;
+    font-size: 2rem;
+    padding: 0.8rem;
+    color: var(--black);
+    border-radius: 6px;
+    outline: none;
+    border: none;
+  }
+  .projects__searchBar .searchIcon {
+    position: absolute;
+    width: 2rem;
+    right: 1rem;
+  }
 `;
 
 export default function Projects() {
