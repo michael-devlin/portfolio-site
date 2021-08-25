0255c6b3494ebb3e456441e36cb3a781ede27207
mikailbadoula <michaeldevfay@gmail.com>
2021-08-25 09:56:57 +0100

    add form submission for contact me page

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -46,43 +46,49 @@ export default function ContactForm() {
   return (
     <>
       <FormStyle>
-        <div className="form-group">
-          <label htmlFor="name">
-            Your Name
-            <input
-              type="text"
-              id="name"
-              name="name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-          </label>
-        </div>
-        <div className="form-group">
-          <label htmlFor="email">
-            Your Email
-            <input
-              type="email"
-              id="email"
-              name="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </label>
-        </div>
-        <div className="form-group">
-          <label htmlFor="message">
-            Your message
-            <textarea
-              type="text"
-              id="message"
-              name="message"
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
-            />
-          </label>
-        </div>
-        <button type="submit">Send</button>
+        <form
+          action="mailto:michaeldevfay@gmail.com"
+          method="post"
+          encType="text/plain"
+        >
+          <div className="form-group">
+            <label htmlFor="name">
+              Your Name
+              <input
+                type="text"
+                id="name"
+                name="name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </label>
+          </div>
+          <div className="form-group">
+            <label htmlFor="email">
+              Your Email
+              <input
+                type="email"
+                id="email"
+                name="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </label>
+          </div>
+          <div className="form-group">
+            <label htmlFor="message">
+              Your message
+              <textarea
+                type="text"
+                id="message"
+                name="message"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+              />
+            </label>
+          </div>
+          <button type="submit">Send</button>
+        </form>
       </FormStyle>
     </>
   );
