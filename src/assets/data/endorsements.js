bf4cae4d9cc10f1eb66e6734ae33f9590d833081
michael-devlin <michaeldevfay@gmail.com>
2022-11-04 11:26:50 +0000

    add endorsement

--- src/assets/data/endorsements.js
+++ src/assets/data/endorsements.js
@@ -1,14 +1,22 @@
 const endorsements = [
   {
     id: 1,
-    name: 'Saki Reid',
-    title: 'Software Developer',
-    org: 'Ori',
+    name: 'Jesse Yuen',
+    title: 'Lead Designer',
+    org: 'Made Tech',
     desc:
-      'Michael is a great teammate with really good energy. He’s innovative and generates lots of good ideas for project name and designs. He’s very smart and lots of fun to work with and fantastic communication skills.',
+      'Michael came onto the Ofqual project for 4 weeks to support and help facilitate a round of usability testing. I was impressed by his enthusiasm to take on the challenge, communication skills and ability to adapt and immerse themselves in a new team, and confidence with facilitating the usability testing sessions.',
   },
   {
     id: 2,
+    name: 'Saki Reid',
+    title: 'Software Developer',
+    org: 'Freelance/Founders & Coders',
+    desc:
+      'Michael is a great teammate with really good energy. He’s innovative and generates lots of good ideas for project name and designs. He’s very smart and lots of fun to work with and has fantastic communication skills.',
+  },
+  {
+    id: 3,
     name: 'Jo Humphrey',
     title: 'Software Developer',
     org: 'Freelance/Founders & Coders',
@@ -19,7 +27,7 @@ const endorsements = [
     id: 3,
     name: 'Chun Zhang',
     title: 'Software Developer',
-    org: 'Tortoise Media',
+    org: 'Freelance/Founders & Coders',
     desc:
       'Michael is a great team player and brings energy to the team. He is talented at coming up with ideas for projects.',
   },
